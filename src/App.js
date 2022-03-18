9d0d5e9dbaaef5518e20f726ef4a50625f2eab74
Elliot Erickson <eric0952@gmail.com>
2022-03-18 12:18:59 -0700

    static app frame

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import "./index.css"
+
+function App() {
+  return (
+    <div className="App">
+        <div className="calculator-container">
+          <div className="display-container">
+
+          </div>
+          <div className="button-container">
+
+          </div>
+        </div>
+    </div>
+  );
+}
+
+export default App;
